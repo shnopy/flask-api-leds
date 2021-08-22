3d7cf021c54c5378be7eef2348b6cbc3d0f7ce93
sticehurst369 <sticehurst@jtsr.co.uk>
2021-08-22 22:25:37 +0100

    wip

--- weather_lights/weather-lights.sh
+++ weather_lights/weather-lights.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
-
 . config
 
-
-TEMP=`curl -s ${HOSTS[temp]}/sensor/$SENSOR |jq .value`
-LEDSTATUS=`./status`
-
-echo $TEMP
-echo $LEDSTATUS
-
-if [ $LEDSTATUS -eq 0 ]
+if [ `./sunset` ] && [ $HOUR -gt 15 ]
 then
-    echo "turning LEDs on"
-#     resp=`curl -s -X POST ${HOSTS[leds]}/on/ | jq .response | tr -d '"'`
-#     echo $resp
-#     if [ $resp = 'LEDs on' ]
-#     then
-#         echo "?error - cannot turn LEDs on"
-#         exit;
-#     fi
+    if [ `./status` -eq 0 ]
+    then
+        echo `./onoff on`
+    fi
+
+
+    TEMP=`curl -s ${HOSTS[temp]}/sensor/$SENSOR |jq .value`
+    if [ $TEMP -gt 15 ]
+    then
+        ./setCol ${WEATHER[vHot]}
+    else
+        if [ $TEMP -gt 10 ]
+        then
+            ./setCol ${WEATHER[normal]}
+        else
+            ./setCol ${WEATHER[vCold]}
+        fi
+    fi
+
+else
+    if [ `./status` -eq 1 ]
+    then
+        echo `./onoff`
+    fi
 fi
+
+
+
+
+# LEDSTATUS=`./status`
+
+# echo $TEMP
+# echo $LEDSTATUS
+
+
+
